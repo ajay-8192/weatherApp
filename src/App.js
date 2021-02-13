ac824f494aa75746e0206caf4c04f22dec168900
ajay-8192 <ajaychowdary3699@gmail.com>
2021-02-13 10:19:12 +0530

    Weather details per 3 hours for 5 days

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import https from 'https';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const api_key = 'd5dffb4be1ef4856e606fa1d6044221c'
+const place='Kanigiri';
+const unit = "metric";
+// const api_place = `https://api.openweathermap.org/data/2.5/weather?q=${place}&units=${unit}&appid=${api_key}`
+const api_5place = `https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${api_key}&units=${unit}`
+
+
+// console.log(weatherapp)
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      weather: []
+    }
+  }
+
+  componentDidMount() {
+    https.get(api_5place, (resp) => {
+      console.log(resp.statusCode);
+  
+      resp.on('data', data => {
+          const weatherData = JSON.parse(data);
+          
+          const weatherapp = []
+          for (let weather of weatherData["list"]) {
+            const temp = weather.main.temp;
+            const weatherDescription = weather.weather[0].description;
+            const icon = weather.weather[0].icon;
+            const imageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+            const date_txt = weather.dt_txt
+            
+            weatherapp.push({
+              temp: temp,
+              weatherDescription: weatherDescription,
+              imageURL: imageURL,
+              date_txt: date_txt
+            })
+          }
+          console.log(weatherapp);
+          this.setState({
+              weather: [...weatherapp]
+          })
+      })
+  })
+  }
+
+  render() {
+    const { weather } = this.state;
+    console.log(this.state);
+    const details = weather.map(element => 
+        <div key={element}>
+          <h1>The temperature in {place} is {element.temp} deg celsius.</h1>
+          <h3>The weather is currently {element.weatherDescription}</h3>
+          <img src={element.imageURL} alt="weather condition" />
+          <h3>{element.date_txt}</h3>
+        </div>
+    )
+    return (
+      <div className="App">
+        {details}
+      </div>
+    );
+  }
 }
 
 export default App;
