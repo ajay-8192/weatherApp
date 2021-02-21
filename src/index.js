4942471a750593cdbbb2a8832f48ed4ae7500ab6
ajay-8192 <ajaychowdary3699@gmail.com>
2021-02-21 12:51:31 +0530

    Weather app for every 3 hours for 5 days

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+Sentry.init({
+  dsn: "https://f2346bcb91a343ee93e6fb24de962ce5@o528509.ingest.sentry.io/5645905",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
+
 ReactDOM.render(
   <React.StrictMode>
+    {/* <button onClick={methodDoesNotExist}>Break the world</button>; */}
     <App />
   </React.StrictMode>,
   document.getElementById('root')
